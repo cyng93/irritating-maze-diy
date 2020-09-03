7b2e40dea0d0cb8f9508a8d3ba10fb912909d7ea
NG CHING YI <cyng93+github@gmail.com>
2020-09-03 22:44:36 +0800

    Reviewed and release to NewLife

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 let 失敗次數 = 0
 input.onButtonPressed(Button.A, function () {
-    music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
     失敗次數 = 0
+    music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
 })
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P2) == 1) {
