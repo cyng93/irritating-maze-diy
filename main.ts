62c76e4dc9ee3c638d8ab0ee0a97c29aac3c6219
NG CHING YI <cyng93+github@gmail.com>
2020-09-28 18:44:23 +0800

    remove master

--- main.ts
+++ main.ts
@@ -7,7 +7,5 @@ basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P2) == 1) {
         music.playTone(262, music.beat(BeatFraction.Half))
         basic.showIcon(IconNames.No)
-        失敗次數 += 1
-        basic.showNumber(失敗次數)
     }
 })
